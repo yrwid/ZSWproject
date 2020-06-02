60871def2f2edcf5a448603f036b3c5a4b0f2cee
yrwid <57493850+yrwid@users.noreply.github.com>
2020-06-02 07:46:15 +0200

    new main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#main -------------------------------------------------------------------------------------------------------------------------
-displayExample()
-#seed = "user inpt"
-seed = 1
-solveJS(seed,'all','html','gantt')
-#solveJS(seed,'all','raw','raw')
+#---------------------------------------main-----------------------------------------------
+foo.displayExample()                                    #example for user input
+#seed = "user inpt"                                     #seed for user input 
+seed = 1                                                #seed for random generate
+foo.solveJS(seed,'lpt','html','gantt')                  #solve JS problem with one method, html table format and draw gantt
+#foo.solveJS(seed,'lpt','raw','raw')                    #solve JS problem with one method, raw input data and raw solution
+#foo.solveJS(seed,'all','html','gantt')                 #solve JS problem using all methods, generata html table and draw gantt
